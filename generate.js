b5bdb4fbf76558e7d15bb20b17a6e4253589e340
Jye Karl-Perry <me@jye.guru>
2021-12-29 02:12:39 +1100

    Initial Commit

---
+++ generate.js
@@ -0,0 +1,58 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+
+let releases_url = "https://api.github.com/repos/cleverraven/cataclysm-dda/releases";
+let changes_url = "https://api.github.com/repos/cleverraven/cataclysm-dda/pulls?state=closed&sort=created&direction=desc&per_page=100";
+
+let settings = { method: 'GET' };
+
+let releases
+let changes
+
+const getReleases = async function() {
+    if (!releases) {
+        const data = await fetch(releases_url, settings);
+        releases = await data.json();
+    }
+    return releases;
+}
+
+const getChanges = async function() {
+    if (!changes) {
+        const data = await fetch(changes_url, settings);
+        changes = await data.json();
+    }
+    return changes;
+}
+
+const initData = async function() {
+    await getReleases();
+    await getChanges();
+}
+
+const generateChanges = async function() {
+    let markdown = "";
+    let plaintext = "";
+
+    await initData();
+    for (const change of changes) {
+        let release = releases.find(x => x.target_commitish === change.merge_commit_sha);
+        if (release) {
+            markdown += `\n---\n\n#### ${release.name} ([${release.target_commitish.substring(0,7)}](${release.html_url}))\n\n`;
+            plaintext += `\n${release.name} (${release.target_commitish.substring(0,7)})\n`;
+        }
+        markdown += `* ${change.title} ([#${change.number}](${change.html_url}))\n`;
+        plaintext += `* ${change.title} (#${change.number})\n`;
+    }
+    return {
+        markdown: markdown,
+        plaintext: plaintext
+    }
+}
+
+generateChanges()
+    .then((output) => {
+        fs.writeFileSync(path.resolve("./changelog.txt"), output.plaintext)
+        fs.writeFileSync(path.resolve("./changelog.md"), output.markdown)
+    });
